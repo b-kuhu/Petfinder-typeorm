882232fbc29d3a671d43e5a87d74c2d7d9802bc5
b-kuhu <Kuhu Bhatnagar>
2022-08-24 11:49:14 +0530

    add ownerSchema for validation

--- src/validationSchema.ts
+++ src/validationSchema.ts
@@ -7,3 +7,10 @@ const animalSchema = joi.object({
            .required(),
   breed:joi.string().min(3).required()         
 })
+
+const ownerSchema = joi.object({
+    owner_name:joi.string() .required().min(3).max(20),
+    contact:joi.number().required().min(10).max(11),
+    address:joi.string().required().min(3).max(30)           
+})
+
