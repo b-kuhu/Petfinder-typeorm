77ac7fea005864672e102953449c29b16d4bd37b
b-kuhu <Kuhu Bhatnagar>
2022-08-24 11:39:22 +0530

    add animalSchema for validation

---
+++ src/validationSchema.ts
@@ -0,0 +1,9 @@
+const joi = require('joi');
+
+const animalSchema = joi.object({
+  name:joi.string()
+           .min(3)
+           .max(20)
+           .required(),
+  breed:joi.string().min(3).required()         
+})
